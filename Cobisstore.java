d700f6d210c9eabe709c67cd068c8a7eeee4d100
kirebeasley <55118075+kirebeasley@users.noreply.github.com>
2020-12-08 13:28:04 -0500

    Create Cobisstore.java

---
+++ Cobisstore.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Cobisstore {
+    private String product;
+    private double price;
+    private int inventory;
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
